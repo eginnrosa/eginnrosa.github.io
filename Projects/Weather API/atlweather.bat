21ca6638ffc7bcf34661ac6770d6f042afe068c9
ekrosa <ekginn2@gmail.com>
2019-05-19 13:41:50 -0400

    edited directory name, removed weather api

--- Projects/Weather API/atlweather.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\EKR\Anaconda3\python.exe" "C:\Users\EKR\Documents\Career\Portfolio\eginnrosa.github.io\Projects\Weather API\atlweather.py"
-pause
--- Projects/Weather API/config.py
+++
@@ -1,1 +0,0 @@
-api_key = "bb3501fdf448f7f81b0ea754e12ee1ff"
